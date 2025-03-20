738b7a1c78f4432aa99cc29655d14b321414329e
Ruhul <ruhulthisis@gmail.com>
2025-03-20 20:33:16 +0600

    get courses from db

--- service/mongo.js
+++ service/mongo.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export async function dbConnect(){
     try {
-        const conn = await mongoose.connect(String(process.env.MONGODB_CONNECTION_STRING));
+        const conn = await mongoose.connect(String(process.env.MONGODB_CONNECTION_URI));
         return conn;
     } catch(err) {
         console.error(err);
