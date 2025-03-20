c45ce4997e370ed2b75137d65df3658f22a78f7f
Ruhul <ruhulthisis@gmail.com>
2025-03-20 20:41:32 +0600

    get category details by ref in course data

--- queries/course.js
+++ queries/course.js
@@ -1,15 +1,16 @@
 import { Course } from "@/model/course-model";
-// import { Category } from "@/model/category-model";
+import { Category } from "@/model/category-model";
 // import { User } from "@/model/user-model";
 // import { Testimonial } from "@/model/testimonial-model";
 // import { Module } from "@/model/module.model";
 
 export async function getCourses() {
     const courses = await Course.find({})
+    .populate({
+        path: "category",
+        model: Category
+    })
     // .populate({
-    //     path: "category",
-    //     model: Category
-    // }).populate({
     //     path: "instructor",
     //     model: User
     // }).populate({
