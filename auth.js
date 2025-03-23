4bec07a4cc4bc2580120d8b91fd2bebe3e679f33
Ruhul <ruhulthisis@gmail.com>
2025-03-23 22:22:31 +0600

    add JWT callback for authentication and update public routes to include Google auth callback

--- auth.js
+++ auth.js
@@ -54,5 +54,10 @@ export const {
                 },
             },
         }),
-    ]
+    ],
+    callbacks: {
+        async jwt({ token, user,account }) {
+            
+        }
+    },
 });
