dfcc9790f0e19e48c961e32cb1a4077b9b53c422
Ruhul <ruhulthisis@gmail.com>
2025-05-13 21:32:59 +0600

    module-17 complete

--- app/dashboard/courses/[courseId]/modules/[moduleId]/_components/video-url-form.jsx
+++ app/dashboard/courses/[courseId]/modules/[moduleId]/_components/video-url-form.jsx
@@ -71,7 +71,7 @@ export const VideoUrlForm = ({ initialData, courseId, lessonId }) => {
   return (
     <div className="mt-6 border bg-slate-100 rounded-md p-4">
       <div className="font-medium flex items-center justify-between">
-        Video URL
+        Video  URL
         <Button variant="ghost" onClick={toggleEdit}>
           {isEditing ? (
             <>Cancel</>
